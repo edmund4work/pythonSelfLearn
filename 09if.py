0deb799a452225d42eb72bcf42dcbbe28018e088
edmund4work <edmund4work@users.noreply.github.com>
2023-06-26 21:56:36 +0800

    new stuff

--- 09if.py
+++ 09if.py
@@ -1,12 +1,19 @@
-def where2eat(mood) :
+def where2eat(mood, money) :
     eatAt = ""
-    if mood == "good" :
+    if mood == "good" and float(money)>100 :
         eatAt = "mamak"
-    elif mood=="bad":
-        eatAt   = "mcd"
+    elif mood=="bad" and float(money)>= 80 :
+        eatAt = "mcd"
+    elif mood == "good" or float(money) < 80 :
+        eatAt = "steamboat"
+    elif mood != "good" and not(float(money) < 100) :
+        eatAt = "buffet"
     else :
         eatAt = "home"
 
     return eatAt
 
 
+a = input("mood good?")
+b = input("how much you have?")
+print(where2eat(a,b))
