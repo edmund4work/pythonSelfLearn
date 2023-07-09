b7935f8df53fb154f9ff0bb1d4da22a96b8143ec
edmund4work <edmund4work@users.noreply.github.com>
2023-07-09 23:05:34 +0800

    update

--- 16_readWriteFile.py
+++ 16_readWriteFile.py
@@ -8,10 +8,21 @@ file = open("16_readWriteFile.txt",mode="r")
 print (file.readline()) #read 1 line
 print(seperateStr)
 print (file.read()) #read all
-
-
 file.close()
+
 
 print(seperateStr)
 with open('16_readWriteFile.txt', 'r') as G:
     print(G.read())
+
+print(seperateStr)
+with open('16_readWriteFile.txt', 'w') as G:
+    G.write("a new line with mode w") #due to mode is 'w', so only overwrite. 
+
+print(seperateStr)
+with open('16_readWriteFile.txt', 'a') as G:
+    G.write("\na new line with mode a") #if wanna write append the file, can try this. mode = a
+
+print(seperateStr)
+with open('16_readWriteFile.txt', 'a', encoding='utf-8') as G:
+    G.write("\na 中文 mode a") #if wanna write chinese or non-english append the file, can try this. mode = a
---
+++ 17_module.py
@@ -0,0 +1,9 @@
+#get other page function.
+
+a = __import__('08_function')
+import token
+import sys
+
+a.calculater(1,2)
+
+print(sys.path)
